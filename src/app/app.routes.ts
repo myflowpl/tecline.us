71d6f47cb7c9be9d06ba4f69e6140e681c91f4eb
myflowpl <pedro.blaszczak@gmail.com>
2025-02-14 11:22:53 +0100

    layout html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./home/home.page').then(m => m.HomePage),
+    },
+    {
+        path: 'catalog',
+        loadComponent: () => import('./catalog/catalog.page').then(m => m.CatalogPage),
+    }
+];
