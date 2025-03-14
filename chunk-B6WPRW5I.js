b7b08c9ece1f34388cb6debd1c9d0b606d915426
myflowpl <pedro.blaszczak@gmail.com>
2025-03-14 21:29:33 +0100

    Auto-generated commit

---
+++ chunk-B6WPRW5I.js
@@ -0,0 +1,1 @@
+import{Ab as i,Ob as s,gb as a,zb as o}from"./chunk-C5FS32FS.js";var n=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=a({type:t,selectors:[["app-category-list"]],decls:2,vars:0,template:function(e,c){e&1&&(o(0,"p"),s(1,"category-list works!"),i())},encapsulation:2})};export{n as CategoryListPage};
