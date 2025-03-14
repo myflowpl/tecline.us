9e7a234bba40dd7ea882f78a716b2e383971b07a
myflowpl <pedro.blaszczak@gmail.com>
2025-03-14 12:35:50 +0100

    main menu responsive

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, MatIconModule, MatBadgeModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
 export class LayoutComponent {
-
+  cart = inject(CartService);
 }
